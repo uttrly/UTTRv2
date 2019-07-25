ce53be75a52e1d398ddbd46ed0d3c62584b6915f
Sarah Sakhri <sarah.sakhri@gmail.com>
2019-07-25 12:59:37 -0400

    creategoal page added, form slider needed

--- client/src/pages/Main/index.js
+++ client/src/pages/Main/index.js
@@ -1,34 +1,34 @@
 import React from 'react';
-import { MDBContainer, MDBRow, MDBCol, MDBBtn, } from 'mdbreact';
+import { MDBContainer, MDBRow, MDBCol, MDBBtn } from 'mdbreact';
 import '../pageStyle.css'
 import target from '../../image/target.jpg'
 
 
 class Main extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
     };
   }
 
   render() {
-      return(
-        <MDBContainer className="text-center mt-5 pt-5 mainContainer">
-          <MDBRow className="align-items-center">
-            <MDBCol size="md-4 sm-12" className="ml-auto">
-              <h1>Be UTTR-ly Amazing</h1>
-              <hr/>
-              <p>You want to be better? But your future self lets you down? We are here to help.</p>
-              <br></br>
-              <MDBBtn color="warning" href="/about">Learn More!</MDBBtn>
-            </MDBCol>
-            <MDBCol>
-              <img src={target} alt="logo" className="logoImg"></img>
-            </MDBCol>
-          </MDBRow>
-        </MDBContainer>
-      );
-    }
+    return (
+      <MDBContainer className="text-center mt-5 pt-5 mainContainer">
+        <MDBRow className="align-items-center">
+          <MDBCol size="md-4 sm-12" className="ml-auto">
+            <h1>Be UTTR-ly Amazing</h1>
+            <hr />
+            <p>You want to be better? But your future self lets you down? We are here to help.</p>
+            <br></br>
+            <MDBBtn color="warning" href="/about">Learn More!</MDBBtn>
+          </MDBCol>
+          <MDBCol>
+            <img src={target} alt="logo" className="logoImg"></img>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+    );
+  }
 };
 
 export default Main
---
+++ client/src/pages/createGoal/index.js
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
+
+
+class CreateGoal extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+        }
+    }
+
+    render() {
+        return (
+
+            <MDBContainer className="mt-5 pt-5 mainContainer">
+                <MDBRow>
+                    < MDBCol lg="10" className="mx-auto">
+                        <div class="card card-signin flex-row my-5">
+                            <div class="card-body">
+                                <h3 class="card-title text-center">Create a Challenge</h3>
+                                <form id="goalForm" >
+                                    <div class="form-group">
+                                        <label>Title : </label>
+                                        <input type="name" class="form-control" id="title" aria-describedby="goalTitle" placeholder="Enter Title" Required />
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Description : </label>
+                                        <textarea class="form-control" id="description" Required></textarea>
+                                    </div>
+                                    <fieldset class="form-group">
+                                        <div class="row">
+                                            <legend class="col-form-label col-md-3 pt-0">Commitment Type : </legend>
+                                            <div class="form-check col-md-2">
+                                                <input class="form-check-input" type="radio" name="goalType" id="onGoing" value="false" checked />
+                                                <label class="form-check-label">On Going</label>
+                                            </div>
+                                            <div class="form-check col-md-2">
+                                                <input class="form-check-input" type="radio" name="goalType" id="oneShot" value="true" />
+                                                <label class="form-check-label">One Shot</label>
+                                            </div>
+                                        </div>
+                                    </fieldset>
+
+                                    <div class="form-group">
+                                        <label for="inputStartDate">Start Date :</label>
+                                        <input class="form-control" type="date" id="startDate" Required />
+                                    </div>
+
+                                    <div class="form-group" id="durationField" >
+                                        <label for="duration">Duration :</label>
+                                        <input class="form-control" type="number" placeholder="(in weeks)" id="duration" value="0" />
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="refereeEmail">Referee Email Address :</label>
+                                        <input type="email" class="form-control" id="refEmail" aria-describedby="emailHelp" placeholder="Enter Referee Email" Required />
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="stake">Stake :</label>
+                                        <input type="file" accept="image/*" class="form-control" id="stake" Required />
+                                    </div>
+
+                                    <button type="submit" class="btn btn-dark">Submit</button>
+                                </form>
+                            </div>
+                        </div>
+                    </MDBCol>
+                </MDBRow>
+            </MDBContainer>
+
+        );
+    };
+}
+
+
+export default CreateGoal;
