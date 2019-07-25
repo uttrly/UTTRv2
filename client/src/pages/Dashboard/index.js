b20cb5e1fde0aad83c3c371280f68f96c2c8f6dd
Phuoc Phan <phuocphan12@gmail.com>
2019-07-24 21:49:08 -0400

    dashboard layout is done, working on challange page

---
+++ client/src/pages/Dashboard/index.js
@@ -0,0 +1,106 @@
+import React from 'react';
+import { MDBContainer, MDBRow, MDBCol, MDBBtn, } from 'mdbreact';
+import '../pageStyle.css'
+import target from '../../image/target.jpg'
+
+
+class Dashboard extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+    };
+  }
+
+
+
+  render() {
+
+    let style = {
+        cursor:"pointer"
+      }
+
+      return(
+        <MDBContainer className="mt-5 pt-5 mainContainer text-dark">
+            <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                <h1 className="h1">Dashboard</h1>
+                <div className="btn-toolbar mb-2 mb-md-0">
+                </div>
+            </div>
+
+            <h2 className="h2">UTTR Points</h2>
+
+            <MDBRow>
+                <MDBCol lg="4" md="6">
+                    <div className="account-points-update panel panel-primary mouseover-animated-pulse click-animated-jello" style={style}>
+                    <div className="panel-heading">
+                        <div className="row">
+                        <div className="col-sm-3"><i className="fas fa-bolt fa-5x"></i></div>
+                        <div className="col-sm-9 text-right">
+                            <div className="account-points-earned huge text-right">PlaceHOLDER</div>
+                            <div>Earned Points!</div>
+                            <h6>Click to enter to win a car</h6>
+                        </div>
+                        
+                        </div>
+                    </div>
+                    </div>
+                </MDBCol>
+            </MDBRow>
+
+
+            <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                <h2 class="h2">My Goals</h2>
+                <div class="btn-toolbar mb-2 mb-md-0">
+                <div class="btn-group mr-2">
+                    <MDBBtn class="btn btn-sm btn-outline" onclick="location.href='/dashboard';"><i class="fas fa-th-list"></i>My Goals</MDBBtn>
+                    <MDBBtn class="btn btn-sm btn-outline" onclick="location.href='/dashboard/referee';"><i class="far fa-eye"></i>Referee</MDBBtn>
+                </div>
+
+                <div class="btn-group mr-2">
+                    <MDBBtn class="btn btn-sm btn-outline" onclick="location.href='/createGoal';"><i class="far fa-plus-square"></i> Add New Goal</MDBBtn>
+                </div>
+                </div>
+            </div>
+
+
+            <MDBRow>
+                <MDBCol md="2">
+                <ul class="nav flex-column">
+                    <li class="nav-item">
+                    <a class="nav-link active" href="/dashboard">
+                        <i class="fas fa-chart-line"></i>
+                        Active <span class="sr-only">(current)</span>
+                    </a>
+                    </li>
+                    <li class="nav-item">
+
+                    <a class="nav-link" href="/dashboard/complete">
+
+                        <i class="far fa-check-square"></i> Complete
+                    </a>
+                    </li>
+                </ul>
+                </MDBCol>
+                <MDBCol md="10">
+
+                <div class="list-group scrollable">
+
+                    <a href="/challenge/{{id}}" class="list-group-item list-group-item-action flex-column align-items-start">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h5 class="mb-1 indigo-text">Place holder for title</h5>
+                        <small class="cyan-text">1/10</small>
+                    </div>
+                    <p class="mb-1">here is where you put the description for each tasks</p>
+                    <small>Created: Today Date</small>
+                    </a>
+
+
+                </div>
+                </MDBCol>
+            </MDBRow>
+        </MDBContainer>
+      );
+    }
+};
+
+export default Dashboard
