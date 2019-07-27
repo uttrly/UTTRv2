dcf940d48ead272ce954508a13ab8a9bb67d2f57
Phuoc Phan <phuocphan12@gmail.com>
2019-07-27 16:00:17 -0400

    fixing merge

--- models/user.js
+++ models/user.js
@@ -1,61 +1,28 @@
 module.exports = function (sequelize, DataTypes) {
-  //Allowing Null for everything but email as a `User` may be created when referee email is provided.
-  var User = sequelize.define("user", {
-    id: {
-      autoIncrement: true,
-      primaryKey: true,
-      type: DataTypes.INTEGER
-    },
-    firstName: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    lastName: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    about: {
-      type: DataTypes.TEXT
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        isEmail: true
-      }
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    // phoneNumber: {
-    //   type: DataTypes.INTEGER,
-    //   allowNull: true
-    // },
-    last_login: {
-      type: DataTypes.DATE
-    },
-    status: {
-      type: DataTypes.ENUM('active', 'inactive'),
-      defaultValue: 'active'
-    },
-
-  });
-
-  User.associate = function (models) {
-    User.belongsToMany(models.Goal, {
-      through: {
-        model: "userGoals",
-        unique: false
+    //Allowing Null for everything but email as a `User` may be created when referee email is provided.
+    var User = sequelize.define("User", {
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          isEmail: true
+        }
       },
-      foreignKey: "UserId",
-    })
+      password: {
+        type: DataTypes.STRING,
+      },
+    });
+  ​
+    User.associate = function (models) {
+      User.belongsToMany(models.Goal, {
+        through: {
+          model: "userGoals",
+          unique: false
+        },
+        foreignKey: "UserId",
+      })
+    };
+  ​
+  ​
+    return User;
   };
-
-
-  return User;
-};
